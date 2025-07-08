6b39e234cb6a444fab2deaffeaab033d83f9832d
lvspa <166564473+lvspa@users.noreply.github.com>
2025-07-08 18:12:50 -0300

    modelos de conexao

---
+++ Models/Produtos.cs
@@ -0,0 +1,9 @@
+public class Produtos
+{
+    public int id { get; set; }
+    public int quantidade { get; set; }
+    public decimal valor { get; set; }
+    public int pedidoid{ get; set; }
+
+
+}
